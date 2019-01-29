8f3a8f02850f6aa50875a4d407b6ff21682fb335
Stephen Afolabi <stephenafolabi@gmail.com>
2019-01-29 17:23:13 +0000

    home and account apps functioning

---
+++ accounts/urls.py
@@ -0,0 +1,19 @@
+from accounts.views import login, register_seller, register_buyer, profile, logout
+from django.conf.urls import url, include
+from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
+from django.core.urlresolvers import reverse_lazy
+
+urlpatterns = [
+    url(r'^login$', login, name='login'),
+    url(r'^login/seller$', register_seller, name='register_seller'),
+    url(r'^login/buyer$', register_buyer, name='register_buyer'),
+    url(r'^profile/$', profile, name='profile'),
+    url(r'^logout/$', logout, name='logout'),
+    url(r'^password-reset/$', password_reset,
+        {'post_reset_redirect': reverse_lazy('password_reset_done')}, name='password_reset'),
+    url(r'^password-reset/done/$', password_reset_done, name='password_reset_done'),
+    url(r'^password-reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm,
+        {'post_reset_redirect': reverse_lazy('password_reset_complete')}, name='password_reset_confirm'),
+    url(r'^password-reset/complete/$', password_reset_complete, name='password_reset_complete'),
+    
+]
--- privatetutor/settings/base.py
+++ privatetutor/settings/base.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_forms_bootstrap',
     'storages',
+    'home',
+    'accounts',
 ]
 
 MIDDLEWARE = [
